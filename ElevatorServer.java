04df119bb5bfbb2eeb3cfdb2c4e79f37a4b1f6cc
Hasan Issa <hasan.issa@carleton.ca>
2019-02-01 13:05:28 -0500

    Elevator Server CLass

--- ElevatorServer.java
+++ ElevatorServer.java
@@ -1,4 +1,14 @@
+import java.net.DatagramSocket;
 
 public class ElevatorServer {
 
+	private int portNumber;
+	private DatagramSocket serverSocket;
+	
+	private void sendCurrentFloorNumberAndElevatorDirection(int currentFloorNumber, Direction direction) {
+		
+	}
+	private void sendElevatorButtonRequest(int floorNumber) {
+		
+	}
 }
