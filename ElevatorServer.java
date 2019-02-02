857b1b34e9d2bc1ab7a06a8ae736aeaaeb4d67ff
george-carleton <georgehajjar@cmail.carleton.ca>
2019-02-02 13:22:54 -0500

    Implementing methods withing ElevatorServer

--- ElevatorServer.java
+++ ElevatorServer.java
@@ -5,13 +5,17 @@
  * The elevator software/controller that is responsible for 
  * communicating over the network and processing elevator commands 
  */
+import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
 
 public class ElevatorServer {
 
 	private int portNumber;
 	private DatagramSocket serverSocket;
+	private DatagramPacket elevatorInfo;
 	
 	public ElevatorServer(int portNumber) {
 		this.portNumber = portNumber;
@@ -25,7 +29,28 @@ public class ElevatorServer {
 	
 	private void sendCurrentFloorNumberAndElevatorDirection(int currentFloorNumber, Direction direction) {
 		
+		byte[] message = new byte[100];
+		message[0] = (byte)currentFloorNumber;
+		
+		//message[1] = (byte)direction;
+		
+		int messageLength = message.length;
+		
+		try {
+			elevatorInfo = new DatagramPacket(message, messageLength, InetAddress.getLocalHost(), portNumber);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+		
+		try {
+			serverSocket.send(elevatorInfo);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
+	
 	private void sendElevatorButtonRequest(int floorNumber) {
 		
 	}
